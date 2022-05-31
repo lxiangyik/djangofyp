0b085e1062473cd4e83108c945cfed12bd355c66
lxiangyik <assassinsboston@gmail.com>
2022-05-31 14:14:50 +0800

    setting up api folders files, urlspath done, and database connection

--- APFAST/views.py
+++ APFAST/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def hello(request): 
+    return HttpResponse('Hello World')
+
+
+# def index(request):
+#     return render(request, 'hello.html',{'name' : "Mosh"})
+
+
+# def room(request):
+#     rooms = Room.objects.all()
+#     return render(request, 'home.html', {'rooms' : rooms})
+ 
+
+# def deleteRoom(request,pk):
+#     selectedRoom = Room.objects.get(id=pk)
+#     selectedRoom.delete()
+#     return redirect('../')
--- FYP/settings.py
+++ FYP/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'FYP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'APFAST',
+        'USER': 'root',
+        'PASSWORD': 'FYPreactzzz',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
